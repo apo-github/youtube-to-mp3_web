01d9e561a2691168e27f86c9acd6065cbc4ebcae
apo-github <fukick@gmail.com>
2024-02-07 16:31:36 +0900

    gunicorn導入

--- static/func.js
+++ static/func.js
@@ -1,3 +1,17 @@
+//初期ロード画面
+window.onload = function() {
+    const spinner = document.getElementById('loading');
+    spinner.classList.add('loaded');
+}
+
+// //待機画面 
+// jQuery(window).on("load", function() {
+// 	// jQuery("body").attr("data-loading", "true");
+//     const spinner = document.getElementById('loading');
+//     spinner.classList.add('loaded');
+// });
+
+// URL確認処理
 function clearText() {
 	let url_form = document.getElementById("url");
     url_form.value = '';
@@ -25,14 +39,14 @@ function submitClick(){
         alert("Please enter a valid URL (´・ω・`)");
         clearText();
         return false; 
-    }
-
-    if (url_text.includes('@')){ //チャンネルの場合
+    }else if (url_text.includes('@')){ //チャンネルの場合
         alert("Sorry, Channel URL is not available (´・ω・`)");
         clearText();
         return false; 
     }
 }
+
+
 
 let url = document.getElementById('url');
 url.addEventListener('change', isValidUrl); // イベントが発生した時の処理
